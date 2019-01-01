3b888a4b97e59ea99881a4953e8621d5798cff96
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2019-01-01 01:43:25 +0100

    adding class to read object from file

---
+++ src/ex1/ReadObjectFromFile.java
@@ -0,0 +1,31 @@
+package ex1;
+
+import java.io.*;
+
+public class ReadObjectFromFile {
+    public static void main(String[] args) {
+        Object obj = null;
+        TestUser user = null;
+        File file = new File("/Users/marinasantana/Desktop/user.txt");
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            try {
+                ObjectInputStream os = new ObjectInputStream(fis);
+                try {
+                    obj = os.readObject();
+                    user = (TestUser) obj;
+                    System.out.println("id: " + user.id);
+                    System.out.println("name: " + user.name);
+                    System.out.println("level: " + user.level);
+                } catch (ClassNotFoundException c) {
+                    System.err.println(c.getMessage());
+                }
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
