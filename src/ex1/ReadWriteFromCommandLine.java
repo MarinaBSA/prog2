fc981d91867e668276bce877f02e05278681e977
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2019-10-26 20:32:55 +0200

    program made to learn about reading/writing from/to command line

---
+++ src/ex1/ReadWriteFromCommandLine.java
@@ -0,0 +1,20 @@
+package ex1;
+
+import java.io.*;
+
+public class ReadWriteFromCommandLine {
+    public static void main(String[] args) {
+        final String finishComm = "exit";
+        try {
+            System.out.println("insert something: ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String line = null;
+
+            while(!(line = br.readLine()).equals(finishComm)) {
+                System.out.println("result: " + line);
+            }
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
