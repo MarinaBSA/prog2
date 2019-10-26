efe3d083da2f38f10c80525a0faaa0c549f784b3
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2019-10-26 20:30:13 +0200

    chat can now read/write to files - bug free

--- src/ex1/CmdlineUI.java
+++ src/ex1/CmdlineUI.java
@@ -1,129 +1,128 @@
 package ex1;
 
+import com.sun.codemodel.internal.JForEach;
+
 import java.io.*;
-
-import org.omg.CORBA.DataInputStream;
-//import java.util.Arrays;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.io.OutputStream;
-
+import java.util.Arrays;
+import static java.lang.System.*;
 
 public class CmdlineUI {
-	
-	public static void main(String[] args) {
-		CmdlineUI ui = new CmdlineUI();
-		ui.readCommands();	
+	//TODO create test UI/Chat -> insert text, expect something else
+	//TODO append user input to file instead of overwriting it
+	//TODO removing some prints -> replacing them with string messages
+	//TODO catch case for when user inserts an unknown command in the method verifyUserInput
+	//TODO if user input is empty throw except/or handle case
+	//TODO organize throws/exceptions
+
+	public ChatImpl chat;
+	private String message;
+	private String[] rawMessageArray;
+	private String command;
+
+	public String getCommand() {
+		return command;
 	}
-	
-	private void readCommands() {
+
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	public String[] getRawMessageArray() {
+		return rawMessageArray;
+	}
+
+	public void setRawMessageArray(String[] rawMessageArray) {
+		this.rawMessageArray = rawMessageArray;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String rawMessage) {
+		this.message = rawMessage;
+	}
+
+	CmdlineUI(){
+		chat = new ChatImpl();
+	}
+
+	public void readCommands() throws Exception {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		
-		String commandLineString = null;
-		String[] commands = null;
+
+		String commandLineString = "";
 		boolean shellIsRunning = true;
-		
-		//Creating file and inserting user's input
-		OutputStream file = createFile("testing.txt");
+
 		try {
 			System.out.println("Enter exit to finish");
-
 			while(shellIsRunning) {
-				System.out.println("> ");
+				System.out.print("> ");
 				commandLineString = br.readLine();
-				commands = parsingCommands(commandLineString);
-				//System.out.println("read from command line: " + commandLineString);
-				shellIsRunning = verifyUserInput(commands);
-				if (!shellIsRunning) { continue; }
-				writingToFile(file, commands);	
+				this.setMessage(parseInput(commandLineString));
+				shellIsRunning = verifyUserInput();
 			}
-			//read from file
-			File fileToRead = new File("/Users/marinasantana/eclipse-workspace/Prog2/testing.txt");
-			String fileContent = readFile(fileToRead);
-			System.out.println("Content: " + fileContent);
-			
+
 		} catch(IOException e) {
 			System.err.println("exception when reading from command line: " + e.getLocalizedMessage());
 		}
 	}
-	
-	//Extracting each word from the users input. Separation by blank space
-	private String[] parsingCommands(String commands) {
-		String[] result =  commands.split(" ");
-		return result;
+
+	//Separating command from the actual message out of the user's input.
+	private String parseInput(String input) {
+		//TODO: Do I really need this split("") and trim() ?
+		String[] rawMessageArray = input.trim().split(" ");
+		this.setRawMessageArray(rawMessageArray);
+
+		String command = rawMessageArray[0];
+		this.setCommand(command);
+
+		String message = String.join(" ", Arrays.copyOfRange(rawMessageArray, 1, rawMessageArray.length));
+		this.setMessage(message);
+		return message;
 	}
-	
-	private boolean verifyUserInput(String[] input) {
-		final String stopCommand = "exit"; 
-		String command = input[0].trim(); 
-		//Removing the first element of the array(the command)
-		//String[] usersInput = Arrays.copyOfRange(input, 1, input.length);
+
+	private boolean verifyUserInput() throws Exception {
+		String command = this.getCommand();
+		String message = this.getMessage();
+		final String stopCommand = "exit";
+		final String write = "write";
+		final String read = "read";
+		final String finishedMessage = "Exit.";
+		final String writingMessage = "Writing...";
+		final String readingMessage = "Reading...";
+
 		switch(command) {
-			//case "exit":
 			case stopCommand:
-				System.out.println("Bye.");
+				out.println(finishedMessage);
 				return false;
+			case write:
+				out.println(writingMessage);
+				chat.writeMessage(message);
+				return true;
+			case read:
+				out.println(readingMessage);
+				this.showReceivedMessage(chat.readMessage());
+				return true;
 			default:
 				return true;
 		}
 	}
-	
-	private OutputStream createFile(String filename) {
-		try {
-			OutputStream os = new FileOutputStream(filename);
-			System.out.println("File successfully created.");
-			return os;
-			// brutal exception handling
-		} catch (FileNotFoundException ex) {
-			System.err.println("couldn’t open file - fatal.");
-			System.exit(0); 
-			return null;
+
+	private void showReceivedMessage(String[] messages) {
+		out.println("File's Content: ");
+		for (String word : messages) {
+			out.println(word);
 		}
 	}
-	
-	private void writingToFile(OutputStream file, String[] usersInput) {
-		byte[] usersInputBytes = null;
-		for (String word : usersInput) {
-			word = word + " ";
-			usersInputBytes = word.getBytes();
-			try {
-				file.write(usersInputBytes);
-			} catch(IOException ex) {
-				System.err.println("could not write data.");
-				System.exit(0);
-			}
-		}
-		System.out.println("Text successfully written to file.");
-	}
-	
-	private String readFile(File file) {
-		String fileContent = null;
+
+	public static void main(String[] args) {
+		CmdlineUI ui = new CmdlineUI();
 		try {
-			FileInputStream fis = new FileInputStream(file);
-			try {
-				InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
-				try {
-					BufferedReader bf = new BufferedReader(isr);
-					//while(bf.readLine() != null) { 
-						fileContent = bf.readLine();	
-					//}
-					return fileContent;
-				} catch(IOException e) {
-					System.err.println("file could not be read");
-					return "";
-				}
-			} catch(UnsupportedEncodingException e) {
-				System.err.print("format not supported");
-				System.exit(0);
-				return "";
-			}
-		} catch(FileNotFoundException e) {
-			System.err.println("file could not be found");
-			System.exit(0);
-			return "";
-		}					
-		
+			ui.readCommands();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
 
