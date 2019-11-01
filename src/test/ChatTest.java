b13fc40803f6f0d26836799ad1c9614b65a6f96e
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2019-11-01 15:38:13 +0100

    finished tests - testing all 3 commands and the 2 methods on the interface

--- src/test/ChatTest.java
+++ src/test/ChatTest.java
@@ -1,15 +1,93 @@
 import ex1.Chat;
+import ex1.ChatImpl;
+import ex1.CmdlineUI;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ChatTest{
-    ChatTest() {
+import java.io.*;
+import java.util.Arrays;
 
+public class ChatTest{
+    private Chat chat;
+    private CmdlineUI UI;
+    final private String messageToWrite1 = "Houston we have a problem";
+    final private String messageToWrite2 = "Test 1 2 3 - end of test";
+
+
+    public ChatTest() {
+        chat = new ChatImpl();
+        UI = new CmdlineUI();
     }
 
     @Test
-    public void testWrongInput()  {
+    public void testFileOutput() throws Exception  {
+        chat.writeMessage(this.messageToWrite1);
+        String[] receivedMessage = chat.readMessage();
+
+        Assert.assertTrue(Arrays.asList(receivedMessage).contains(this.messageToWrite1));
     }
 
+    @Test
+    public void testWrongFileOutput() throws Exception {
+        final String wrongMessage = "You need people like me";
 
+        chat.writeMessage(this.messageToWrite1);
+        String[] receivedMessage = chat.readMessage();
 
+        try {
+            Assert.assertFalse(Arrays.asList(receivedMessage).contains(wrongMessage));
+        } catch (AssertionError e) {
+            System.err.println("Text written in file is not the expected one");
+        }
+    }
+
+    @Test
+    public void UITestWriteCommand() throws Exception {
+        String shownMessage = UITestHelper(CmdlineUI.WRITECOMMAND, this.messageToWrite2);
+        try {
+            Assert.assertEquals(shownMessage, CmdlineUI.WRITINGMESSAGE);
+        } catch (AssertionError e) {
+            System.err.println("Could not see the writing message after inserting the write command");
+        }
+    }
+
+    @Test
+    public void UITestReadCommand() throws Exception {
+        String shownMessage = UITestHelper(CmdlineUI.READCOMMAND, this.messageToWrite2);
+        try {
+            Assert.assertEquals(shownMessage, CmdlineUI.READINGMESSAGE);
+        } catch (AssertionError e) {
+            System.err.println("Could not see the reading message after inserting the read command");
+        }
+    }
+
+    @Test
+    public void UITestExitCommand() throws Exception {
+        String shownMessage = UITestHelper(CmdlineUI.EXITCOMMAND, this.messageToWrite2);
+        try {
+            Assert.assertEquals(shownMessage, CmdlineUI.EXITMESSAGE);
+        } catch (AssertionError e) {
+            System.err.println("Could not see the exit message after inserting the exit command ");
+        }
+    }
+
+    //Helper methods///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private String UITestHelper(String command, String messageToWrite) throws Exception {
+        InputStream is = null;
+
+        //Write something in an array of bytes
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream outputStream = new DataOutputStream(baos);
+        outputStream.writeUTF(command + " " + messageToWrite);      //"write" exists now in an array of bytes coded as UTF inside baos
+
+        byte[] inputBytes = baos.toByteArray();
+        is = new ByteArrayInputStream(inputBytes);
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        UI.runUI(is, os);
+
+        return os.toString().trim();     //reading back whatever was written by my output stream in CmdlineUI
+    }
 }
