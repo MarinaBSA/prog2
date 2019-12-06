cbbbc479cde9f9c058583da660069d95735c110e
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2019-12-06 12:18:29 +0100

    Changed chat's interface to send and read message through a TCP server instead of to/from a file

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -1,8 +1,10 @@
 package chat;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public interface Chat {
-    void writeMessage(String message) throws Exception;
+    void writeMessage(String message, OutputStream os) throws Exception;
 
-    String[] readMessage() throws Exception;
-
+    String readMessage(InputStream is) throws Exception;
 }
